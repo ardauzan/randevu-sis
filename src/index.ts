6469e73d150cfe383bd05c8e0498915e2610a434
Ali Arda Uzan <arda.uzan@protonmail.com>
2024-05-09 15:50:59 +0300

    w

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,57 @@
 import { Pool } from 'pg'
 import veritabanınıOluştur from '@/veritabanı'
-import sunucuyuAyağaKaldır from '@/sunucu'
+import arkayüz from '@/sunucu'
 import {
   trueVeyaFalseuBaşarılıyaDönüştür,
   uygulamaBirSüreÇalıştıktanSonraKapatıldı
 } from '@/sunucu/kütüphane'
 import type { NodePgDatabase } from 'drizzle-orm/node-postgres'
 import type * as şema from '@/veritabanı/şema'
+import bilgiler from '../package.json'
+
+//# Bu yazılılmla ilgili npm paket bilgilerini al ve dışa aktar
+const {
+  name,
+  version,
+  description,
+  license,
+  author,
+  repository,
+  engines: { bun }
+} = bilgiler
 
 //# Ortamı kontrol et
-export const uygulamaVersiyonu = '0.1.0'
-const desteklenenBunVersiyonu = '1.1.7'
+export const paketAdı = name
+export const uygulamaVersiyonu = version
+export const uygulamanınAdı = description
+export const lisans = license
+export const yazar = author
+export const depo = repository
+export const desteklenenBunVersiyonu = bun
+
+//# İmza
+console.info(
+  `Paket ismi ${paketAdı} olan ve güncel versiyonu (v${uygulamaVersiyonu}) olan (${uygulamanınAdı}) adlı yazılım;`
+)
+console.info(
+  `2024 yılında, detayları | ${yazar} |`,
+  `olan kişi tarafından | ${lisans} | lisansı altında yayınlanmıştır.`
+)
+console.info(
+  "Süleyman Demirel Üniversitesi için geliştirilmiştir. Üniversite bünyesindeki Prototip Atölyesi'nin randevularını yönetmek için tasarlanmıştır."
+)
+console.info('Bu yazılım aktif geliştirme aşamasındadır. Dikkatli kullanınız.')
+console.info(`| ${depo} | adresindeki depoda barınır.`)
+console.info(
+  'Bu yazılım 64-bit x86 mimarisi çalıştıran Linux 5.6 ve üzeri işletim sistemlerinde çalışır.',
+  'Bun ın en son çıkan versiyonuyla devamlı test edilir ve çalışma ortamı olarak Bun en güncel hali hedeflenir. Bun dışında çalıştırılmamalıdır. (Node veya Deno ile çalıştırılmamalıdır.)'
+)
+
+console.info('Başlıyor...')
+
 console.info('Ortam Bun mı? Kontrol ediliyor...')
 if (typeof Bun === 'undefined') {
-  console.error('Ortam Bun değil. Uygulama başlatılamaz!')
+  console.error('Ortam Bun değil. Uygulama başlatılamaz')
   uygulamaBirSüreÇalıştıktanSonraKapatıldı()
   process.exit(1)
 }
@@ -25,7 +63,7 @@ if (şuanKiBunVersiyonu === desteklenenBunVersiyonu)
   )
 else {
   console.error(
-    `Bun versiyon ${şuanKiBunVersiyonu} ortamında uygulama başlatılamaz, ${desteklenenBunVersiyonu} gereklidir!`
+    `Bun versiyon ${şuanKiBunVersiyonu} ortamında uygulama başlatılamaz, ${desteklenenBunVersiyonu} gereklidir`
   )
   uygulamaBirSüreÇalıştıktanSonraKapatıldı()
   process.exit(1)
@@ -119,7 +157,7 @@ giriş.success &&
 bulunamadı.success
   ? console.info('Önyüz oluşturuldu.')
   : (() => {
-      console.error('Önyüz oluşturulamadı!')
+      console.error('Önyüz oluşturulamadı.')
       uygulamaBirSüreÇalıştıktanSonraKapatıldı()
       process.exit(1)
     })()
@@ -135,7 +173,7 @@ if (
   !process.env['JWT_SECRET'] ||
   !process.env['COOKIE_SECRET']
 ) {
-  console.error('Ortam değişkenleri eksik. Onlar olmadan devam edilemez!')
+  console.error('Ortam değişkenleri eksik. Onlar olmadan devam edilemez.')
   uygulamaBirSüreÇalıştıktanSonraKapatıldı()
   process.exit(1)
 }
@@ -156,15 +194,17 @@ try {
   process.exit(1)
 }
 console.info('Veritabanına bağlanıldı.')
+arkayüz.listen(3000)
+console.info(`SDÜ Randevu Yönetim Sistemi v(${uygulamaVersiyonu}) çevrimiçi.`)
 
-//# Sunucuyu ayağa kaldır
-console.warn('Sunucu ayağa kaldırılıyor...')
-try {
-  sunucuyuAyağaKaldır()
-} catch (hata: any) {
-  console.error('Sunucu ayağa kaldırılırken bir hata oluştu:', hata.message)
+//# Linux sinyallerini dinle ve uygulamayı gerektiği gibi durdur, yeniden başlat yada kapat
+process.on('SIGINT', () => {
+  console.info('SIGINT sinyali alındı. Sunucu kapatılıyor...')
+  arkayüz.stop()
+  console.info('Sunucu kapatıldı.')
+  console.info(
+    `SDÜ Randevu Yönetim Sistemi v(${uygulamaVersiyonu}) çevrimdışı.`
+  )
   uygulamaBirSüreÇalıştıktanSonraKapatıldı()
-  process.exit(1)
-}
-
-console.info(`SDÜ Randevu Yönetim Sistemi v(${uygulamaVersiyonu}) çevrimiçi!`)
+  process.exit(0)
+})
--- src/sunucu/index.ts
+++ src/sunucu/index.ts
@@ -1,67 +1,84 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
 import { logger } from '@bogeychan/elysia-logger'
 import pino from 'pino'
 import { serverTiming } from '@elysiajs/server-timing'
 import { swagger } from '@elysiajs/swagger'
 import { staticPlugin } from '@elysiajs/static'
 import { jwt } from '@elysiajs/jwt'
-import { uygulamaBirSüreÇalıştıktanSonraKapatıldı } from '@/sunucu/kütüphane'
-import { uygulamaVersiyonu } from '@/index'
 
-export default function sunucuyuOluştur() {
-  const arkayüz = new Elysia({ name: 'arkayüz' })
-    .onStart(() => {
-      console.warn('Arkayüz çalışmaya başladı.')
-    })
-    .use(
-      logger({
-        stream: pino.multistream([
-          process.stdout,
-          pino.destination('günlük.log')
-        ]),
-        transport: {
-          target: 'pino-pretty',
-          options: {
-            colorize: true
-          }
-        }
+const cookieİleBeraber = new Elysia({
+  name: 'cookieİleBeraber',
+  cookie: {
+    httpOnly: true,
+    path: '/',
+    sameSite: 'strict',
+    secure: true,
+    priority: 'high',
+    sign: ['session']
+  }
+}).guard({
+  cookie: t.Cookie({
+    session: t.Optional(
+      t.Object({
+        session: t.String()
       })
     )
-    .use(
-      serverTiming({
-        allow: true,
-        trace: { total: true }
-      })
-    )
-    .use(swagger({ provider: 'swagger-ui' }))
-    .use(staticPlugin({ assets: 'statik', prefix: '/statik' }))
-    .use(
-      jwt({
-        name: 'jwt',
-        secret: process.env['JWT_SECRET']!
-      })
-    )
-    .get('/', (ctx) => {
-      ctx.log.error(ctx, 'Context')
-      ctx.log.info(ctx.request, 'Request')
-      return 'pino-pretty'
-    })
-    .listen(3000)
-    .onStop(() => console.warn('Arkayüz çalışmayı durdurdu!'))
-
-  console.warn(
-    `Sunucu ayakta. Detaylar: ${arkayüz.server?.hostname}:${arkayüz.server?.port}`
-  )
-
-  //# Linux sinyallerini dinle ve uygulamayı gerektiği gibi durdur, yeniden başlat yada kapat
-  process.on('SIGINT', () => {
-    console.warn('SIGINT sinyali alındı. Sunucu kapatılıyor...')
-    arkayüz.stop()
-    console.warn('Sunucu kapatıldı.')
-    console.info(
-      `SDÜ Randevu Yönetim Sistemi v(${uygulamaVersiyonu}) çevrimdışı!`
-    )
-    uygulamaBirSüreÇalıştıktanSonraKapatıldı()
-    process.exit(0)
   })
-}
+})
+
+const kimlikVerisiİleBeraber = new Elysia({ name: 'kimlikVerisiİleBeraber' })
+  .use(cookieİleBeraber)
+  .guard(
+      {
+        
+        beforeHandle: isSignIn
+      },
+      (app) =>
+        app
+          .resolve(({ cookie: { session } }) => ({
+            userId: findUserById(session.value)
+          }))
+          .get('/profile', ({ userId }) => userId)
+    )
+  })
+
+const arkayüz = new Elysia({ name: 'arkayüz' })
+  .onStart(() => {
+    console.info('Arkayüz çalışmaya başladı.')
+  })
+  .use(
+    logger({
+      stream: pino.multistream([
+        process.stdout,
+        pino.destination('günlük.log')
+      ]),
+      transport: {
+        target: 'pino-pretty',
+        options: {
+          colorize: true
+        }
+      }
+    })
+  )
+  .use(
+    serverTiming({
+      allow: true,
+      trace: { total: true }
+    })
+  )
+  .use(swagger({ provider: 'swagger-ui' }))
+  .use(staticPlugin({ assets: 'statik', prefix: '/statik' }))
+  .use(
+    jwt({
+      name: 'jwt',
+      secret: process.env['JWT_SECRET']!
+    })
+  )
+  .get('/', (ctx) => {
+    ctx.log.error(ctx, 'Context')
+    ctx.log.info(ctx.request, 'Request')
+    return 'pino-pretty'
+  })
+  .onStop(() => console.info('Arkayüz çalışmayı durdurdu.'))
+
+export default arkayüz
